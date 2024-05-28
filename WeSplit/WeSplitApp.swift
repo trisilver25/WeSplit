91f779c85ce3565901094c059bf1d912b5d5e97b
Tristin Smith <tsmithassoc@gmail.com>
2024-05-28 18:18:50 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Tristin Smith on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
