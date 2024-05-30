34fd0011661369e0a586bc1345dedb08752ab25c
Tristin Smith <tsmithassoc@gmail.com>
2024-05-30 07:46:12 -0400

    Update: - Removed header from Tip percentage section, as it does not need to be repeated twice.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -51,7 +51,7 @@ struct ContentView: View {
                     .pickerStyle(.navigationLink)
                 }
 
-                Section("How much do you want to tip?") {
+                Section() {
                     Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(0..<101) {
                             Text($0, format: .percent)
